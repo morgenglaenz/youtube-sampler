cc32df033125a7f1a9fadac137c87e0607da8ee3
morgenglaenz <morgenglaenz@gmail.com>
2015-02-12 08:51:45 +0100

    auth and merging

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 var search = require('youtube-search');
+var youtdl = require('youtube-dl');
 var ytdl = require('ytdl-core');
 var fs = require('fs');
+var creds = require('./creds.json');
 
 var keyWord = process.argv[2];
+var anon = process.argv[3];
 
 var opts = {
   maxResults: 10,
   startIndex: 1
 };
 
-console.log(keyWord);
-search(keyWord, opts, function (err, results) {
+var searchAndConquer = search(keyWord, opts, function (err, results) {
   if (err) return console.error(err);
   results.forEach(function (result, i) {
-    ytdl(result.url)
+    var video;
+    if (creds.user && creds.pass &&
+        anon !== 'true') {
+      console.log('auth is on for user: ' + creds.user);
+      video = youtdl(result.url, ['-u ' + creds.user, '-p ' + creds.pass])
+    } else {
+      console.log('im downloadings stuff without auth');
+      video = ytdl(result.url);
+    }
+    video
       .pipe(fs.createWriteStream('./video/tmp-' + keyWord + '_' + + i + '.mp4'));
       
   });
--- muxit.js
+++ muxit.js
@@ -1,43 +1,58 @@
 var ffmpeg = require('fluent-ffmpeg');
 var fs = require('fs');
+var Q = require('q');
 var events = require('events');
 var spawn = require('child_process').spawn;
 
 var videos, inputs;
 var output = process.argv[2];
-var output;
+var outfile;
 
 fs.readdir('./video/', function (err, list) {
   videos = list;
   muxing(list);
 });
 
+var promises = [];
 
 var muxing = function (list) {
+  console.log('Getting duration of videos, stripping to 2 sec')
   list.forEach(function (video, i) {
     ffmpeg.ffprobe('./video/' + video, function (err, metadata) {
         var duration = metadata.format.duration;
-        console.log(duration / 2)
-        if (duration !== 'N/A') {
-         output = ffmpeg('./video/' + video).seek(duration / 2).setDuration(2.0)
-            .output('./tmp/' + i + '.mp4')
-            .run()
-        }
-           
-        if (!inputs) {
-          inputs = ffmpeg('./video/' + video)
-        }
-        inputs
-          .seek(duration / 2)
-          .setDuration(2)
-
-          
-       if (videos.length - 1 === i) {
-        inputs.mergeToFile('outputfile.mp4');
-       }
+        var outfile;
+        var promise = Q.defer();
+        promises.push((promise)
+        outfile = ffmpeg('./video/' + video).seek(duration / 2).setDuration(2.0)
+          .output('./tmp/' + i + '.mp4')
+          .on('end', function () {
+            promise.resolve();
+          })
+          .run()
       });
   });
 };
 
+var merger = function () {
+  console.log('Concatenating videos, this takes a while');
+  var concatString = "";
+  fs.readdir('./tmp/', function (err, list) {
+    list.forEach(function (tmp, i) { 
+      concatString += 'file tmp/' + tmp + "\n";  
+    });
+    fs.writeFileSync('input.txt', concatString);
+    concat = spawn('ffmpeg', ['-f', 'concat', '-i', 'input.txt', 'out.mp4']);
+    concat.stderr.on('data', function (data) {
+      console.log('error' + data)
+    });
+    concat.on('close', function (code) {
+      console.log(code);
+    });
+  });     
+  // ffmpeg -f concat -i .txt
  
+};
+
+Q.all(promises)
+  .then(merger);
 
