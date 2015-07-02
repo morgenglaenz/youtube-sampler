36599440cb133b4fa93fabc432948b62f02f2369
morgenglaenz <morgenglaenz@gmail.com>
2015-07-02 11:42:36 +0200

    upload to twi-ah

--- upload.js
+++ upload.js
@@ -1,5 +1,14 @@
 var settings = require('./settings');
 var spawn = require('child_process').spawn;
+var creds = require(settings.dir + './creds');
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: creds.t.key,
+    consumer_secret: creds.t.secret,
+    access_token_key: creds.t.tokenKey,
+    access_token_secret: creds.t.tokenSecret
+});
 //var crypto = require('crypto');
 //var proquint = require('proquint');
 
@@ -15,6 +24,16 @@ module.exports = function () {
 
   upload.stderr.on('data', function (data) {
     console.log(''+data);
+    if ((data + '').indexOf('Video URL:') > -1) {
+      var url = ('' + data).split('Video URL: ')[1];
+      var message = 'Make the #news be a #daily #painkiller #scrape: \n' + url;
+      client.post('statuses/update', {status: message}, function(error, tweet, response){
+          if (!error) {
+                console.log("Tweeting succesful", tweet.id);
+                  }
+          else {console.log(error);}
+      });
+    }
   });
   upload.stdout.on('data', function (data) {
     console.log(''+data);
